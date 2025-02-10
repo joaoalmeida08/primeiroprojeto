ef816dcbd6623b319ac79cf73f5234c49d68d6bc
João Almeida <juanzintv@gmail.com>
2025-02-10 00:57:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int main()
@@ -14,12 +6,12 @@ int main()
     int valor1;
     int valor2;
  
-    printf("digite o valor1 : ");
+    printf("Digite o valor1 : ");
     scanf("%d",& valor1);
     
-    printf("digite o valor2:");
+    printf("Digite o valor2:");
     scanf("%d",& valor2);
     
-    printf("o valor ditado foi:%d",valor1 + valor2);
+    printf("A soma do valor foi:%d",valor1 + valor2);
     return 0;
 }
